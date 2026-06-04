0ec31654e3d52a300e1fee7b02ffd076f4b4c035
Birol Bulut <birolblood@gmail.com>
2026-06-05 00:56:41 +0300

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/serhat-yapi-react/',
+  base: './',
   plugins: [react(), tailwindcss()]
 })
